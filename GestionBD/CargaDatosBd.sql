7c90cd088e47c91b5bb3fb7dd2d7c8e0e1d8a532
eli <123782239+eliasgrolon@users.noreply.github.com>
2024-12-29 18:58:12 -0300

    Scripts para BD
    Scripts de creacion y carga de datos para la bd

---
+++ GestionBD/CargaDatosBd.sql
@@ -0,0 +1,15 @@
+INSERT INTO productos (codigoBarra, nombre, marca, categoria, precio)
+VALUES
+('10001', 'Arroz', 'La Moderna', 'Alimentos', 25.50),
+('20002', 'Cereal', 'Kellogg', 'Alimentos', 75.00),
+('30003', 'Shampoo', 'Pantene', 'Higiene Personal', 85.99),
+('40004', 'Jabón', 'Dove', 'Higiene Personal', 35.00),
+('50005', 'Laptop', 'HP', 'Electrónicos', 15000.00),
+('60006', 'Televisor', 'Samsung', 'Electrónicos', 12000.00),
+('70007', 'Café', 'Nescafé', 'Alimentos', 95.50),
+('80008', 'Bicicleta', 'BMX', 'Deportes', 4500.00),
+('90009', 'Zapatillas', 'Nike', 'Deportes', 2200.00),
+('10010', 'Perfume', 'Chanel', 'Cuidado Personal', 2500.00);
+
+
+select * from productos
